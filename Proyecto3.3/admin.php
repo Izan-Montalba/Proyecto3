b617d11c6297f60209ef2152341a9a8114011108
Izan-Montalba <izamonale@alu.edu.gva.es>
2026-01-27 11:24:22 +0100

    Delete Proyecto3.3 directory

--- Proyecto3.3/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['usuario_id']) || $_SESSION['rol'] !== 'admin') {
-    header("Location: index.php");
-    exit();
-}
-// Carga la vista del administrador
-require_once './app/views/admin_view.php';
